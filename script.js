f4fcfe7855762599eda48f7ca9a0acf1a80faa03
joey-largent <177733313+joey-largent@users.noreply.github.com>
2024-12-26 11:32:02 +0300

    create dynamic sizing and functional grid

--- script.js
+++ script.js
@@ -4,15 +4,57 @@ const grid = document.createElement("div");
 grid.classList.add("masterGrid");
 body.appendChild(grid);
 
-//CREATE GRID + DYNAMIC SIZING
-let gridSize = 16
-let gridWidth = 300; // Width of .masterGrid
-let cellSize = gridWidth / gridSize;
+//BRUSHES
 
-for (let i = 0; i < (gridSize * gridSize); i++) {
-    let divs = document.createElement("div");
-    divs.classList.add("divCell");
-    divs.style.width = `${cellSize}px`;
-    divs.style.height = `${cellSize}px`;
-    grid.appendChild(divs);
+const largeBrush = document.createElement("button");
+grid.classList.add("largeBrush");
+body.appendChild(largeBrush);
+
+const medBrush = document.createElement("button");
+grid.classList.add("medBrush");
+body.appendChild(medBrush);
+
+const smallBrush = document.createElement("button");
+grid.classList.add("smallBrush");
+body.appendChild(smallBrush);
+
+//CREATE GRID + DYNAMIC SIZING + HOVER
+let gridSize = 16;
+let gridWidth = 300;
+
+function createGrid() {
+    grid.innerHTML = ""
+    let cellSize = gridWidth / gridSize;
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let divs = document.createElement("div");
+        divs.classList.add("divCell");
+        divs.style.width = `${cellSize}px`;
+        divs.style.height = `${cellSize}px`;
+        grid.appendChild(divs);
+    };
+
+    divs.addEventListener('mouseover', () => {
+        divs.style.backgroundColor = 'black';
+    });
 };
+
+//RESIZE GRID
+largeBrush.addEventListener('click', (event) => {
+    gridSize = 16;
+    createGrid();
+});
+
+medBrush.addEventListener('click', (event) => {
+    gridSize = 50;
+    createGrid();
+});
+
+smallBrush.addEventListener('click', (event) => {
+    gridSize = 100;
+    createGrid();
+});
+
+
+//HOVER COLOR
+
+
