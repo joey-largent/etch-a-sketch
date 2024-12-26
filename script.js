3c969c4d9e04dfa75ec4d343e742fa86dcf8509a
joey-largent <177733313+joey-largent@users.noreply.github.com>
2024-12-26 12:15:28 +0300

    create random color function and simple toggle

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//change base theme colors to a gentle dark pastel
+//add label for brush size indicator
+//add space between brush sizes
+//style reset button
+//place all buttons in a panel to the left
+//make grid responsive
+
+
 const body = document.querySelector("body");
 
 const grid = document.createElement("div");
@@ -5,7 +13,6 @@ grid.classList.add("masterGrid");
 body.appendChild(grid);
 
 //BRUSHES
-
 const largeBrush = document.createElement("button");
 largeBrush.classList.add("largeBrush");
 body.appendChild(largeBrush);
@@ -17,13 +24,6 @@ body.appendChild(medBrush);
 const smallBrush = document.createElement("button");
 smallBrush.classList.add("smallBrush");
 body.appendChild(smallBrush);
-
-//RESET BUTTON
-const resetBtn = document.createElement("button");
-grid.classList.add("resetBtn");
-resetBtn.textContent = "RESET";
-body.appendChild(resetBtn);
-
 
 //CREATE GRID + DYNAMIC SIZING + HOVER
 let gridSize = 16;
@@ -40,7 +40,7 @@ function createGrid() {
         divs.style.height = `${cellSize}px`;
 
         divs.addEventListener('mouseover', () => {
-            divs.style.backgroundColor = 'black';
+            divs.style.backgroundColor = isRandomColor ? getRandomColor() : 'black';
         });
 
         grid.appendChild(divs);
@@ -50,6 +50,29 @@ function createGrid() {
 };
 
 createGrid();
+
+//RESET BUTTON
+const resetBtn = document.createElement("button");
+grid.classList.add("resetBtn");
+resetBtn.textContent = "RESET";
+body.appendChild(resetBtn);
+
+//RANDOM COLOR TOGGLE
+const randomColorToggle = document.createElement("input");
+randomColorToggle.type = "checkbox";
+randomColorToggle.id = "randomColorToggle";
+body.appendChild(randomColorToggle);
+
+let isRandomColor = false;
+
+randomColorToggle.addEventListener('change', () => {
+    createGrid();
+    isRandomColor = randomColorToggle.checked;
+});
+
+function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 //RESIZE GRID
 largeBrush.addEventListener('click', (event) => {
