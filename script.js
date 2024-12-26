b464efa30dd87d11019975a609864df52e0b1b48
joey-largent <177733313+joey-largent@users.noreply.github.com>
2024-12-26 11:06:20 +0300

    create grid with dynamic sizing

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const grid = document.createElement("div");
 grid.classList.add("masterGrid");
 body.appendChild(grid);
 
+//CREATE GRID + DYNAMIC SIZING
+let gridSize = 16
+let gridWidth = 300; // Width of .masterGrid
+let cellSize = gridWidth / gridSize;
 
-for (let i = 0; i < 257; i++) {
+for (let i = 0; i < (gridSize * gridSize); i++) {
     let divs = document.createElement("div");
     divs.classList.add("divCell");
+    divs.style.width = `${cellSize}px`;
+    divs.style.height = `${cellSize}px`;
     grid.appendChild(divs);
 };
-
